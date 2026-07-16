3052754c4960a6a3702b4c491100bead45d01b5c
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2026-07-16 10:38:47 +0100

    Added modularisation.

--- src/1D/critical-points.jl
+++ src/1D/critical-points.jl
@@ -1,23 +1,23 @@
+module CriticalPoints
 
-# using NLsolve
-# using Sobol
-export solve_first_derivative, find_saddles_sobol, find_saddle_similar_seed
+using NLsolve
+using Sobol
 
 function solve_first_derivative(drv::Function, t0::Vector{Float64},
     roundDigits::Int64=5)
 
     try
-    ### using NLsolve
+        ### using NLsolve
         function speqs!(F, x)
-            F[1] = real(drv(x[1]+ x[2]*im))
-            F[2] = imag(drv(x[1]+ x[2]*im))
+            F[1] = real(drv(x[1] + x[2] * im))
+            F[2] = imag(drv(x[1] + x[2] * im))
         end
 
-        result = nlsolve(speqs!, t0)    
+        result = nlsolve(speqs!, t0)
 
         if converged(result)
-            tiSP = result.zero[1] + im*result.zero[2]
-            tiSP = round(tiSP, digits = roundDigits)
+            tiSP = result.zero[1] + im * result.zero[2]
+            tiSP = round(tiSP, digits=roundDigits)
             return tiSP
         else
             return nothing
@@ -29,51 +29,48 @@ function solve_first_derivative(drv::Function, t0::Vector{Float64},
 end
 
 function find_saddles_sobol(drv::Function,
-        tmin::ComplexF64, tmax::ComplexF64,        
-        N::Int64=200 # number of seeds generated per domain
-    ) 
-    
+    tmin::ComplexF64, tmax::ComplexF64,
+    N::Int64=200 # number of seeds generated per domain
+)
+
     roundDigits = 2 # I should certainly change this, it seems a bit excessive
 
     saddles = Vector{ComplexF64}()
-    
-    t_seq = SobolSeq(reim(tmin),reim(tmax))
+
+    t_seq = SobolSeq(reim(tmin), reim(tmax))
 
     for i in 1:N
         t0 = Sobol.next!(t_seq)
         t0 = [t0[1]; t0[2]]
 
-        ts = solve_first_derivative(drv, t0, roundDigits) 
-#         ### check conditons and deposit in array
+        ts = solve_first_derivative(drv, t0, roundDigits)
+        #         ### check conditons and deposit in array
         if !isnothing(ts)
             ts_r = round(ts, digits=roundDigits)
 
             push!(saddles, complex(
-                real(ts_r)==0 ? 0. : real(ts_r),
-                imag(ts_r)==0 ? 0. : imag(ts_r)    
-                ))
+                real(ts_r) == 0 ? 0. : real(ts_r),
+                imag(ts_r) == 0 ? 0. : imag(ts_r)
+            ))
 
         end
     end
 
-    unique!(ts -> round(ts, digits = roundDigits), saddles)
-    sort!(saddles, by = x -> real(x))
+    unique!(ts -> round(ts, digits=roundDigits), saddles)
+    sort!(saddles, by=x -> real(x))
     return saddles
-end;
+end
 
+function find_saddle_similar_seed(drv::Function, ts::ComplexF64; roundDigits=2)
 
+    ts = solve_first_derivative(drv, [reim(ts)...], roundDigits)
 
-function find_saddle_similar_seed(drv::Function, ts::ComplexF64;roundDigits = 2)
-    
-    ts = solve_first_derivative(drv, [reim(ts)...], roundDigits) 
-    
-    if !isnothing(ts) 
+    if !isnothing(ts)
         ts_r = round(ts, digits=roundDigits)
         return ts_r
     else
         return nothing
-    end    
+    end
 end
 
-
-nothing
+end
