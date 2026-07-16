ea428c62f611849aadf37830f03f152e99542c23
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2026-07-16 10:39:48 +0100

    Changed reference from one file to a module.

--- src/PicardLefschetz.jl
+++ src/PicardLefschetz.jl
@@ -2,42 +2,32 @@ module PicardLefschetz
 
 ### make sure they're added to the Project.toml by doing ] activate .,  add PackageName
 using LinearAlgebra
-
-
 ### for finding critical points
 using NLsolve
 using Sobol
-
 ### for the contour intersection
 using Contour
 using StaticArrays
 using GeometryBasics
 
+# 1D code
+include("1D/methods-1D.jl")
 
-### I probably want this at some point
-# import Base: show
+# 2D code
+include("2D/downward-flow-quadrilaterals.jl")
+include("2D/downward-flow-triangles.jl")
+include("2D/flow-utils.jl")
+include("2D/integrate-quads.jl")
+include("2D/integrate-triangles.jl")
+include("2D/necklace-for-dual-thimble.jl")
+include("2D/PLIntegration2D.jl")
+include("2D/saddle-point-method.jl")
+include("2D/saddles-contributing.jl")
+include("2D/saddles-generic.jl")
+include("2D/thimble-from-saddle-point-quadrilaterals.jl")
+include("2D/thimble-from-saddle-point-triangles.jl")
+include("2D/utils.jl")
 
-include("1D/integration-path-flow.jl")
-
-include("1D/critical-points.jl")
-include("1D/line-intersection.jl")
-include("1D/saddle-point-method.jl")
-
-
-
-export square
-
-# Write your package code here.
-function hello() 
-	println("hello, World")
-end
-
-"""
-    square(x)
-
-Returns the square of `x`, for whatever type of argument.
-"""
-square(x) = x^2
-
+export Methods1D
 
 end # PicardLefschetz
