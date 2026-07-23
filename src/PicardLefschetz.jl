b2744de6d63f488a64bb7a152f57a067d86ff4d5
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2026-07-23 12:00:40 +0100

    Exported new modularised unified methods.

--- src/PicardLefschetz.jl
+++ src/PicardLefschetz.jl
@@ -10,12 +10,26 @@ using Contour
 using StaticArrays
 using GeometryBasics
 
+# Types
+include("core/types.jl")
+
 # 1D code
-include("1D/methods-1D.jl")
+include("core/1D/methods-1D.jl")
 
 # 2D code
-include("2D/methods-2D.jl")
+include("core/2D/methods-2D.jl")
 
-export Methods1D, Methods2D
+using Types
+using Methods1D
+using Methods2D
+
+include("core/dual-thimbles.jl")
+include("core/integration.jl")
+include("core/saddles.jl")
+include("core/thimbles.jl")
+
+using .DualThimble, .Integration, .Saddle, .Thimble
+
+export Types, DualThimble, Integration, Saddle, Thimble
 
 end # PicardLefschetz
