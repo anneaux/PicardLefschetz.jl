b314bdc4581accd95eaba743cec685784fefa095
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2026-07-24 23:00:30 +0100

    Added documentation.

--- src/core/dual-thimbles.jl
+++ src/core/dual-thimbles.jl
@@ -7,6 +7,37 @@ using ..Saddle: find_numerical_saddles
 
 # Gets the dual thimble for a given saddle point.
 export get_dual_thimble!
+"""
+    get_dual_thimble!(S, S_grad, S_hessian, saddle_point, params)
+
+Calculates the dual thimble, using gradient ascent to be able to calculate the dual thimble. Gradient ascent is gradient descent in reverse, which is done
+by solving the ODE along a time parameter \$ \\tau \$ to find the dual thimble. The ODE is as such
+\$\$
+\\frac{dS}{d\\tau} = \\overline{\\nabla S(z)}
+\$\$
+
+The parameters for this function are listed in the table:
+
+| Parameter             | Required | Type | Description                                                                                                                                   |
+| --------------------- | -------- | ------ | --------------------------------------------------------------------------------------------------------------------------------------------- |
+| `flow_step_factor`    | Yes      | `Float64` | The step size of the gradient ascent solver.                                                                                                  |
+| `max_iterations`      | Yes      | `Int` | The maximum number of iterations to perform using the solver.                                                                                 |
+| `height_threshold`    | Yes      | `Float64` | The maximal magnitude of the imaginary component during gradient ascent. This is used when checking if the saddle point contributes.        |
+| `init_point_count`    | No       | `Int` | The initial number of points for the dual Lefschetz thimble contour. (This parameter is only required when the dimension of the saddle is 2.) |
+| `init_perturbation_radius` | No  | `Float64` | The initial radius for the dual Lefschetz thimble contour. This is the contour evolved to give the dual thimble. (This parameter is only required when the dimension of the saddle is 2.) |
+| `subdivision_threshold` | No     | `Float64` | The threshold for subdivding the segments between the points of the dual Lefschetz thimble. (This parameter is only required when the dimension of the saddle is 2.) |
+
+
+# Arguments
+- `S::Function`: The action function. 
+- `S_grad::Function`: The gradient of the action function.
+- `S_hessian::Function`: The Hessian of the action function.
+- `saddle_point::Saddle`: The struct containing the saddle point and the related functions.
+- `params::Dict`: The parameters for the dual thimble calculation. All parameters listed above are to be included in this dictionary.
+
+# Returns
+- `Nothing`
+"""
 function get_dual_thimble!(S::Function, S_grad::Function, S_hessian::Function, saddle_point::Saddle, params::Dict)
     flow_step_factor = params["flow_step_factor"]
     max_iterations = params["max_iterations"]
@@ -36,6 +67,38 @@ end
 
 # Gets the dual thimbles for all saddle points.
 export get_dual_thimbles
+"""
+    get_dual_thimbles(S, S_grad, S_hessian, params, domain, contributing)
+
+Calculates the dual thimble, using gradient ascent to be able to calculate the dual thimble. Gradient ascent is gradient descent in reverse, which is done
+by solving the ODE along a time parameter \$ \\tau \$ to find the dual thimble. The ODE is as such
+\$\$
+\\frac{dS}{d\\tau} = \\overline{\\nabla S(z)}
+\$\$
+
+The parameters for this function are listed in the table:
+
+| Parameter             | Required | Type | Description                                                                                                                                   |
+| --------------------- | -------- | ------ | --------------------------------------------------------------------------------------------------------------------------------------------- |
+| `flow_step_factor`    | Yes      | `Float64` | The step size of the gradient ascent solver.                                                                                                  |
+| `max_iterations`      | Yes      | `Int` | The maximum number of iterations to perform using the solver.                                                                                 |
+| `height_threshold`    | Yes      | `Float64` | The maximal magnitude of the imaginary component during gradient ascent. This is used when checking if the saddle point contributes.        |
+| `init_point_count`    | No       | `Int` | The initial number of points for the dual Lefschetz thimble contour. (This parameter is only required when the dimension of the saddle is 2.) |
+| `init_perturbation_radius` | No  | `Float64` | The initial radius for the dual Lefschetz thimble contour. This is the contour evolved to give the dual thimble. (This parameter is only required when the dimension of the saddle is 2.) |
+| `subdivision_threshold` | No     | `Float64` | The threshold for subdivding the segments between the points of the dual Lefschetz thimble. (This parameter is only required when the dimension of the saddle is 2.) |
+
+
+# Arguments
+- `S::Function`: The action function. 
+- `S_grad::Function`: The gradient of the action function.
+- `S_hessian::Function`: The Hessian of the action function.
+- `params::Dict`: The parameters for the dual thimble calculation. All parameters listed above are to be included in this dictionary.
+- `domain::Vector{ComplexDomain}`: The domain over which to calculate the dual thimbles. In the 1D case, the domain should be only one `ComplexDomain`, and in 2D, it should be `ComplexDomain`s.
+- `contributing::Bool`: Whether to include only contributing points. 
+
+# Returns
+- `Vector{Saddle}`: A vector of the saddle points and their dual thimbles.
+"""
 function get_dual_thimbles(S::Function, S_grad::Function, S_hessian::Function, params::Dict, domain::Vector{ComplexDomain}, contributing::Bool)
     saddles = find_numerical_saddles(S_grad, domain, params)
     for saddle in saddles
@@ -54,6 +117,37 @@ end
 
 # Gets the dual thimble boundary for a given saddle point.
 export get_dual_thimble_boundary!
+"""
+    get_dual_thimble_boundary!(S, S_grad, S_hessian, saddle_point, params)
+
+Calculates the boundary of the dual thimble, using gradient ascent to be able to calculate the dual thimble boundary. Gradient ascent is gradient descent in reverse, which is done
+by solving the ODE along a time parameter \$ \\tau \$ to find the dual thimble boundary. The ODE is as such
+\$\$
+\\frac{dS}{d\\tau} = \\overline{\\nabla S(z)}
+\$\$
+
+The parameters for this function are listed in the table:
+
+| Parameter             | Required | Type | Description                                                                                                                                   |
+| --------------------- | -------- | ------ | --------------------------------------------------------------------------------------------------------------------------------------------- |
+| `flow_step_factor`    | Yes      | `Float64` | The step size of the gradient ascent solver.                                                                                                  |
+| `max_iterations`      | Yes      | `Int` | The maximum number of iterations to perform using the solver.                                                                                 |
+| `height_threshold`    | Yes      | `Float64` | The maximal magnitude of the imaginary component during gradient ascent. This is used when checking if the saddle point contributes.        |
+| `init_point_count`    | No       | `Int` | The initial number of points for the dual Lefschetz thimble contour. (This parameter is only required when the dimension of the saddle is 2.) |
+| `init_perturbation_radius` | No  | `Float64` | The initial radius for the dual Lefschetz thimble contour. This is the contour evolved to give the dual thimble. (This parameter is only required when the dimension of the saddle is 2.) |
+| `subdivision_threshold` | No     | `Float64` | The threshold for subdivding the segments between the points of the dual Lefschetz thimble. (This parameter is only required when the dimension of the saddle is 2.) |
+
+
+# Arguments
+- `S::Function`: The action function. 
+- `S_grad::Function`: The gradient of the action function.
+- `S_hessian::Function`: The Hessian of the action function.
+- `saddle_point::Saddle`: The struct containing the saddle point and the related functions.
+- `params::Dict`: The parameters for the dual thimble calculation. All parameters listed above are to be included in this dictionary.
+
+# Returns
+- `Nothing`
+"""
 function get_dual_thimble_boundary!(S::Function, S_grad::Function, S_hessian::Function, saddle_point::Types.Saddle, params::Dict)
     if length(saddle_point) == 1
         saddle = saddle_point.saddle
@@ -84,6 +178,38 @@ end
 
 # Gets the dual thimble boundary for all saddle points.
 export get_dual_thimble_boundaries
+"""
+    get_dual_thimble_boundaries(S, S_grad, S_hessian, params, domain, contributing)
+
+Calculates the boundary of the dual thimbles for all saddles, using gradient ascent to be able to calculate the dual thimble boundary. Gradient ascent is gradient descent in reverse, which is done
+by solving the ODE along a time parameter \$ \\tau \$ to find the dual thimble boundary. The ODE is as such
+\$\$
+\\frac{dS}{d\\tau} = \\overline{\\nabla S(z)}
+\$\$
+
+The parameters for this function are listed in the table:
+
+| Parameter             | Required | Type | Description                                                                                                                                   |
+| --------------------- | -------- | ------ | --------------------------------------------------------------------------------------------------------------------------------------------- |
+| `flow_step_factor`    | Yes      | `Float64` | The step size of the gradient ascent solver.                                                                                                  |
+| `max_iterations`      | Yes      | `Int` | The maximum number of iterations to perform using the solver.                                                                                 |
+| `height_threshold`    | Yes      | `Float64` | The maximal magnitude of the imaginary component during gradient ascent. This is used when checking if the saddle point contributes.        |
+| `init_point_count`    | No       | `Int` | The initial number of points for the dual Lefschetz thimble contour. (This parameter is only required when the dimension of the saddle is 2.) |
+| `init_perturbation_radius` | No  | `Float64` | The initial radius for the dual Lefschetz thimble contour. This is the contour evolved to give the dual thimble. (This parameter is only required when the dimension of the saddle is 2.) |
+| `subdivision_threshold` | No     | `Float64` | The threshold for subdivding the segments between the points of the dual Lefschetz thimble. (This parameter is only required when the dimension of the saddle is 2.) |
+
+
+# Arguments
+- `S::Function`: The action function. 
+- `S_grad::Function`: The gradient of the action function.
+- `S_hessian::Function`: The Hessian of the action function.
+- `params::Dict`: The parameters for the dual thimble calculation. All parameters listed above are to be included in this dictionary.
+- `domain::Vector{ComplexDomain}`: The domain over which to calculate the dual thimbles. In the 1D case, the domain should be only one `ComplexDomain`, and in 2D, it should be `ComplexDomain`s.
+- `contributing::Bool`: Whether to include only contributing points. 
+
+# Returns
+- `Vector{Saddle}`: A vector of the saddle points and their dual thimbles.
+"""
 function get_dual_thimble_boundaries(S::Function, S_grad::Function, S_hessian::Function, params::Dict, domain::Vector{ComplexDomain}, contributing::Bool)
     saddles = find_numerical_saddles(S_grad, domain, params)
     for saddle in saddles
--- src/wrappers/dual-thimbles.jl
+++ src/wrappers/dual-thimbles.jl
@@ -1,6 +1,22 @@
 using Symbolics
 
 export get_dual_thimble!
+"""
+    get_dual_thimble!(z, S, saddle_point, params)
+
+Calculates the dual thimble for a given saddle point using gradient ascent. This is a wrapper 
+function for compatibility with the Symbolics.jl package. For complete documentation, see 
+`get_dual_thimble!`. 
+
+# Arguments
+- `z::Num`: The symbolic variable.
+- `S::Num`: The symbolic action function.
+- `saddle_point::Saddle`: The struct containing the saddle point.
+- `params::Dict`: The parameters for the dual thimble calculation.
+
+# Returns
+- `Nothing`
+"""
 function get_dual_thimble!(
     z::Num, S::Num,
     saddle_point::Saddle,
@@ -16,6 +32,23 @@ function get_dual_thimble!(
 end
 
 export get_dual_thimbles
+"""
+    get_dual_thimbles(z, S, params, domain, contributing)
+
+Calculates the dual thimbles for all saddles using gradient ascent. This is a wrapper 
+function for compatibility with the Symbolics.jl package. For complete documentation, see 
+`get_dual_thimbles`.
+
+# Arguments
+- `z::Num`: The symbolic variable.
+- `S::Num`: The symbolic action function.
+- `params::Dict`: The parameters for the dual thimble calculation.
+- `domain::Vector{ComplexDomain}`: The domain over which to calculate the dual thimbles.
+- `contributing::Bool`: Whether to include only contributing points.
+
+# Returns
+- `Vector{Saddle}`: A vector of the saddle points and their dual thimbles.
+"""
 function get_dual_thimbles(
     z::Num, S::Num, params::Dict,
     domain::Vector{ComplexDomain},
@@ -31,6 +64,22 @@ function get_dual_thimbles(
 end
 
 export get_dual_thimble_boundary!
+"""
+    get_dual_thimble_boundary!(z, S, saddle_point, params)
+
+Calculates the boundary of the dual thimble for a given saddle point using gradient ascent. This is a wrapper 
+function for compatibility with the Symbolics.jl package. For complete documentation, see 
+`get_dual_thimble_boundary!`.
+
+# Arguments
+- `z::Num`: The symbolic variable.
+- `S::Num`: The symbolic action function.
+- `saddle_point::Saddle`: The struct containing the saddle point.
+- `params::Dict`: The parameters for the dual thimble calculation.
+
+# Returns
+- `Nothing`
+"""
 function get_dual_thimble_boundary!(
     z::Num, S::Num,
     saddle_point::Saddle,
@@ -46,6 +95,23 @@ function get_dual_thimble_boundary!(
 end
 
 export get_dual_thimble_boundaries
+"""
+    get_dual_thimble_boundaries(z, S, params, domain, contributing)
+
+Calculates the boundary of the dual thimbles for all saddles using gradient ascent. This is a wrapper 
+function for compatibility with the Symbolics.jl package. For complete documentation, see 
+`get_dual_thimble_boundaries`.
+
+# Arguments
+- `z::Num`: The symbolic variable.
+- `S::Num`: The symbolic action function.
+- `params::Dict`: The parameters for the dual thimble calculation.
+- `domain::Vector{ComplexDomain}`: The domain over which to calculate the dual thimbles.
+- `contributing::Bool`: Whether to include only contributing points.
+
+# Returns
+- `Vector{Saddle}`: A vector of the saddle points and their dual thimbles.
+"""
 function get_dual_thimble_boundaries(
     z::Num, S::Num,
     params::Dict,
