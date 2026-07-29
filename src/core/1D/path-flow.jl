2cc780c9c9257eb4ca642587aec441e298161f0d
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2026-07-29 15:25:34 +0100

    Updated according to changes proposed on issue-#7 by Anne Weber.

--- src/core/1D/path-flow.jl
+++ src/core/1D/path-flow.jl
@@ -152,12 +152,12 @@ end
 
 export flow_up
 function flow_up(S::Function, S_prime::Function, saddle_point::FlowPoint, δ::Float64, h_threshold::Float64, flow_steps::Int)::Tuple{Vector{FlowPoint},Bool}
-    contributing = true
+    contributing = false
     max_height = abs(h_threshold)
     directions = Vector{ComplexF64}()
     get_hessian_eigenvectors!(directions, saddle_point, S, :ascent)
 
-    on_real_line = floor(imag(saddle_point.coords[1])) == 0
+    on_real_line = isapprox(imag(saddle_point.coords[1]), 0.0, atol=1e-10)
 
     if on_real_line
         # Both directions move away from the real line.
@@ -176,7 +176,7 @@ function flow_up(S::Function, S_prime::Function, saddle_point::FlowPoint, δ::Fl
         points_up = flow_line(S, S_prime, pass_up, δ, 1.0, should_stop_infinity, flow_steps)
         points_down = flow_line(S, S_prime, pass_down, δ, 1.0, should_stop_infinity, flow_steps)
 
-        thimble = vcat(reverse(points_down), [saddle_point], points_up)
+        dual_thimble = vcat(reverse(points_down), [saddle_point], points_up)
         contributing = true
     else
         # Determine forward (towards real line) and backward (away from real line) passes
@@ -203,10 +203,10 @@ function flow_up(S::Function, S_prime::Function, saddle_point::FlowPoint, δ::Fl
         should_stop_infinity_backwards(vertices) = -imag(S(vertices)) >= max_height
         points_backward = flow_line(S, S_prime, backward_pass, δ, 1.0, should_stop_infinity_backwards, flow_steps)
 
-        thimble = vcat(reverse(points_backward), [saddle_point], points_forward)
+        dual_thimble = vcat(reverse(points_backward), [saddle_point], points_forward)
     end
 
-    return thimble, contributing
+    return dual_thimble, contributing
 end
 
 
